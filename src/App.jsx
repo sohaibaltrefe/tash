353bc2b830f8c4aad99dc4d3f50620974e87241b
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2023-10-25 01:43:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './assets/compon/navbar/Navbar.jsx'
+import Footer from './assets/compon/footer/Footer.jsx'
+import Prodac from './assets/compon/prodshow/Prodac.jsx'
+
+function App() {
+  return (
+    <div className=' container'>
+    <Navbar />
+<Prodac/>
+    <Footer/>
+    </div>    
+
+  )
+}
+
+export default App
